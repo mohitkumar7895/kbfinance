942c92e9368e96b2e4e29d54154de58c1739648b
Nelson-dayan <nelson.dayan@seclance.com>
2026-09-18 21:09:26 +0530

    jgh

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -156,7 +156,7 @@ export default function ContactPage() {
 
                 <div className="space-y-2">
                   <Label htmlFor="service">Service Required *</Label>
-                  <Select onValueChange={(val) => val && setValue("service", val)} required>
+                  <Select onValueChange={(val: string | null) => { if (val) setValue("service", val); }} required>
                     <SelectTrigger>
                       <SelectValue placeholder="Select a service" />
                     </SelectTrigger>
